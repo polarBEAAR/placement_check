eb5c595609dfc12a1d491b3bde8bf407f14dc5a3
Margarita Mazepa <mmazepa@mirantis.com>
2021-04-16 15:01:29 +0000

    a bunch of small fixed

--- check_placement.sh
+++ check_placement.sh
@@ -5,7 +5,7 @@ exec 2>error_list
 
 rm completed
 
-row="%-8s %-40s %-30s %-10d %-10d %-10d \n"
+row="%-8s %-40s %-30s %-10d %-10d %-10d \n"                                                                ### format string 
 printf "%-8s %-40s %-30s %-10s %-10s %-10s \n" "Host" "Allocation ID" "Result" "RAM" "CPU" "Disk"
 
 memory=0
@@ -13,35 +13,39 @@ cpu=0
 disk=0
 
 
-if !  [ -n "$1" ]
+echo "$@"
+
+if  [ -z "$1" ]
 then
-    list=$(cat /etc/hosts | grep -o "\<cmp0[0-9]*$")
+    list=$(openstack compute service list | grep -o -E "cmp0[0-9]*")
 else
+    #" " read -r -a list <<< "$@"
     list=$@
 fi
 
+echo "$list"
 
 host_list=$(openstack resource provider list)
-for host in $(echo "$list")
+for host in ${list[*]}
 do
-    host_id=$(echo "$host_list" | grep $host | awk '{print $2}')
-    server_list=$(openstack server list --all --host $host -c 'ID')
-    allocation_list=$(openstack resource provider show --allocation $host_id -f yaml)
+    host_id=$(echo "$host_list" | grep "$host" | awk '{print $2}')
+    server_list=$(openstack server list --all --host "$host" -c 'ID')
+    allocation_list=$(openstack resource provider show --allocation "$host_id" -f yaml)
 
     for allocation in $(echo "$allocation_list" | grep -v "uuid"  | grep -E -o "[[:alnum:]]{8}[-]([[:alnum:]]{4}[-]){3}[[:alnum:]]{12}")    ### took all allocation for the host
     do
-        if ! (echo "$server_list" | grep -q $allocation )                                                                                   ### took only allocation which are not presented on the host (origin server was migrated )
+        if ! (echo "$server_list" | grep -q "$allocation" )                                                                                 ### took only allocation which are not presented on the host (origin server was migrated )
         then
-            allocation_output=$(openstack server show $allocation)
+            allocation_output=$(openstack server show "$allocation")
 
-            current_ram=$(echo "$allocation_list" | grep -A 4 $allocation | grep -i memory | awk '{print $2}')
-            let " memory = memory + current_ram "
+            current_ram=$(echo "$allocation_list" | grep -A 4 "$allocation" | grep -i memory | awk '{print $2}')
+            memory=$((memory+current_ram))
 
-            current_cpu=$(echo "$allocation_list" | grep -A 4 $allocation | grep -i cpu | awk '{print $2}')
-            let " cpu = cpu + current_cpu "
+            current_cpu=$(echo "$allocation_list" | grep -A 4 "$allocation" | grep -i cpu | awk '{print $2}')
+            cpu=$((cpu+current_cpu))
 
-            current_disk=$(echo "$allocation_list" | grep -A 4 $allocation | grep -i disk | awk '{print $2}')
-            let "disk = disk + current_disk"
+            current_disk=$(echo "$allocation_list" | grep -A 4 "$allocation" | grep -i disk | awk '{print $2}')
+            disk=$((disk + current_disk))
             
             if ! (echo "$allocation_output" | grep -q host)                                                                                 ### took only allocation which doesn't exist anymore. ( origin server was deleted ) 
             then
@@ -49,10 +53,10 @@ do
             else 
                 result=$(echo "$allocation_output" | grep -w host | awk '{print $4}')
             fi
-            printf "$row" $host $allocation "$result" $current_ram $current_cpu $current_disk
+            printf "$row" "$host" "$allocation" "$result" "$current_ram" "$current_cpu" "$current_disk"
         fi
     done
 done
-printf "%-80s %-10d %-10d %-10d \n" "Total" $memory $cpu $disk
+printf "%-80s %-10d %-10d %-10d \n" "Total" "$memory" "$cpu" "$disk"
 
 touch completed
