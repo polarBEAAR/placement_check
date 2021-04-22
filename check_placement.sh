545678f5d1c995f3be16af7c18a46ae211b1d5ac
Margarita Mazepa <mmazepa@mirantis.com>
2021-04-22 13:44:22 +0000

    Add delete function, but it doesn't delete the allocation from DB. The function generates the deletion command. User is to check them and decide run it or not.
    deletion command list is in delete_list
    all output is in output.log
    all error is in error.log

--- check_placement.sh
+++ check_placement.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-exec 2>error_list
-#exec 1>output
+. /root/keystonercv3      ### contain openstack credentials
+#. ~/delete_placement.sh   ### contain script to delete  allocations. usage: "delete $host_uuid $allocation_uuid"
+#. ~/check_placement.sh    ### contain script to check allocations. usage: "check" - will check all accessible hosts; "check <compute node name>" - will check allocations on specified host
 
-rm completed
+### global var
 
-row="%-8s %-40s %-30s %-10d %-10d %-10d \n"                                                                ### format string 
-printf "%-8s %-40s %-30s %-10s %-10s %-10s \n" "Host" "Allocation ID" "Result" "RAM" "CPU" "Disk"
-
-memory=0
-cpu=0
-disk=0
+exec 1>output.log
+exec 2>error.log
+exec 4>delete_list        ### to avoid enduser-error, the script do not delete the rows from DB. Script generate the list of deletion command only and put it in the file called "delete_list"
 
 
-echo "$@"
+row="%-8s %-40s %-40s %-30s %-10d %-10d %-10d \n"                                                                ### format string
+printf "%-8s %-40s %-40s %-30s %-10s %-10s %-10s \n" "Host" "Host UUID" "Allocation ID" "Result" "RAM" "CPU" "Disk"
+
+### mhost is a var for mysql host adress, mpasswd is a var for mysql password. Both they are required for delete left allocations.
+
+mhost=$(salt-call pillar.get nova:controller:database:host | awk '{print $1}' | grep -v "local")
+mpasswd=$(salt-call pillar.get nova:controller:database:password | awk '{print $1}' | grep -v "local")
+
+### delete function
+
+function delete_plc {
+
+rp_id=$(mysql -sN  -unova -h"$mhost" -p"$mpasswd" -D nova_api -e "select id from resource_providers where uuid='$1'")    ### rp_id - 
+
+#echo "mysql -unova -h"$mhost" -p"$mpasswd" -D nova_api -e \"select * from allocations where consumer_id='$2' and resource_provider_id='$rp_id'\" ">&4
+echo "mysql -unova -h"$mhost" -p"$mpasswd" -D nova_api -e \"delete from allocations where consumer_id='$2' and resource_provider_id='$rp_id'\" ">&4
+}
+
+### generate a list of arguments (hosts)
 
 if  [ -z "$1" ]
 then
-    list=$(openstack compute service list | grep -o -E "cmp0[0-9]*")
+    list=$(openstack compute service list | grep -o -E "cmp0[0-9]*")     ### if there's no arguments script will be run across all compute nodes
 else
-    #" " read -r -a list <<< "$@"
-    list=$@
+    list=("$@")
 fi
 
-echo "$list"
 
-host_list=$(openstack resource provider list)
+### main check
+
 for host in ${list[*]}
 do
-    host_id=$(echo "$host_list" | grep "$host" | awk '{print $2}')
-    server_list=$(openstack server list --all --host "$host" -c 'ID')
+    
+    host_id=$(openstack resource provider list | grep $host | awk '{print $2}')
+    server_list=$(openstack server list --all --host "$host" -c 'ID' )
     allocation_list=$(openstack resource provider show --allocation "$host_id" -f yaml)
 
     for allocation in $(echo "$allocation_list" | grep -v "uuid"  | grep -E -o "[[:alnum:]]{8}[-]([[:alnum:]]{4}[-]){3}[[:alnum:]]{12}")    ### took all allocation for the host
@@ -38,25 +54,16 @@ do
         then
             allocation_output=$(openstack server show "$allocation")
 
-            current_ram=$(echo "$allocation_list" | grep -A 4 "$allocation" | grep -i memory | awk '{print $2}')
-            memory=$((memory+current_ram))
 
-            current_cpu=$(echo "$allocation_list" | grep -A 4 "$allocation" | grep -i cpu | awk '{print $2}')
-            cpu=$((cpu+current_cpu))
 
-            current_disk=$(echo "$allocation_list" | grep -A 4 "$allocation" | grep -i disk | awk '{print $2}')
-            disk=$((disk + current_disk))
-            
-            if ! (echo "$allocation_output" | grep -q host)                                                                                 ### took only allocation which doesn't exist anymore. ( origin server was deleted ) 
+            if ! (echo "$allocation_output" | grep -q host)                                                                                 ### took only allocation which doesn't exist anymore. ( origin server was deleted )
             then
                 result="doesn't exist anymore"
-            else 
+            else
                 result=$(echo "$allocation_output" | grep -w host | awk '{print $4}')
             fi
-            printf "$row" "$host" "$allocation" "$result" "$current_ram" "$current_cpu" "$current_disk"
+            delete_plc "$host_id" "$allocation"                                                                                             ### deletion commands are generated. you can delete all broken allocations run "bash delete_list"
+            printf "$row" "$host" "$host_id" "$allocation" "$result"
         fi
     done
 done
-printf "%-80s %-10d %-10d %-10d \n" "Total" "$memory" "$cpu" "$disk"
-
-touch completed
